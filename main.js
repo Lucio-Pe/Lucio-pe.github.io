74948ab517861fb0dda2c7cbec78018046d739e9
unknown <pereyra600@gmail.com>
2020-11-13 20:09:07 -0300

    clase2

---
+++ main.js
@@ -0,0 +1,19 @@
+let datos = [{id:1, titulo:'carne'},
+{id:2, titulo:'jabon'}];
+
+let btnAgregar = document.querySelector('#btnAgregar');
+let inpBusqueda = document.querySelector('#fixed-header-drawer-exp');
+let btnDeshacer = document.querySelector('#btnDeshacer')
+
+//funciones que necesito
+
+
+
+
+//eventos, hacer click en los botones que tengo: agregar item, deshacer, busqueda
+//limpiar lista, guardaar lista, cargar lista
+
+
+
+
+//objetos
