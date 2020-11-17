cf61ccc23bddcdcfb6fb9d6edd0dd449a27cc7b5
unknown <pereyra600@gmail.com>
2020-11-17 14:40:05 -0300

    final clase3

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+//se comenzo a hacer el script en la clase 2:
+
 let datos = [
-             {id:1, titulo:'carne', cantidad:12, precio:2500},
-             {id:2, titulo:'jabon', cantidad:15, precio:3000}
+             {titulo:'carne', cantidad:12, precio:2500},
+             {titulo:'jabon', cantidad:15, precio:3000}
             ];
 
 
@@ -16,7 +18,7 @@ const linkLimpiarLista = document.querySelector('#linkLimpiarLista');
 
 //funciones que necesito
 
-const template = ({titulo,cantidad,precio})=> {
+const template = ({titulo,cantidad,precio})=> 
 `<div class="item">
 <div class="titulo">
   ${titulo}
@@ -29,7 +31,7 @@ const template = ({titulo,cantidad,precio})=> {
   <a href="#" class="borrar">eliminar</a>
 </div>
 </div>`;
-} //es un template string
+ //es un template string
 function render(lista = [{titulo:'',cantidad:0, precio:0}]){
     galeria.innerHTML = '';
     lista.forEach(item =>{
@@ -37,7 +39,7 @@ function render(lista = [{titulo:'',cantidad:0, precio:0}]){
     });
 }
 
-
+//esto es lo que se agrega al html.
 
 //eventos, hacer click en los botones que tengo: agregar item, deshacer, busqueda
 //limpiar lista, guardaar lista, cargar lista
@@ -83,9 +85,6 @@ linkLimpiarLista.addEventListener('click', ()=>{
     render(datos)
 })
 
-//objetos
-
-
 /*
 vamos dividiendo en partes lo que necesitams para el scrip y darle funcionalidad
 primero agarrar los botones, y luego agrearles eventos a los botones, (click), creamos un objeto
@@ -97,3 +96,63 @@ que es un bucle que ejecuta para cada elemento del array.
 tambien hicimos un una funcion para renderizar, que fuimos agreagando a alguno de los eventos.
 
 **/
+
+
+
+
+//clase 3:
+// en comienzo comenzo resuemiendo los codigos que se hicieron la clase2
+//en esta clase se hace que la app sea instalable y trabjar con alguna api.
+//a partir del minuto 17, comienza a explicar sobre al archivo del manifiesto
+//que toda api que utilice una web de extension tiene que tenr este archivo para que sea instalable
+//el manifiesto de aplicaciones web es un tipo jason(manifest.json), donde tiene las propiedades de nuestra aplicacion
+//tiene, un nombre, una descripcion, iconos, los tamaños que se adapta, coloes de fondo, etc.
+//la clase comenzo a trabajr luego de la explicacion a partir del min 35.
+
+
+//manifiesto:
+//creamos un manifest, pero lo buscamos en la web, como developer manifest.
+//en la pagina de mozilla hay cosas a tener en cuenta para la creacion del manifiesto.
+//hay un posteo de que se llama entiendo el manifiesto de lasaplicaciones web progresivas que
+//explica bien como hay que hacer.(desarrolloweb.com/artiulos/manifest-pregressive-web-apps.html)
+//despues tambien con un generador de manifiestos, web app manifest generator: que se pueden agregar colores, imagenes y lo que por defecto tiene que tener el manifiesto
+//el estart url, se puede pasar por query string, se usa mucho en la parte de service worker(averiguar mas),
+//la diferencia de start ur. y escope, es que start url especifia la url que se carga cuando el usuario lanza la app, y el scope
+//define el ambito de navegacion en el contexto de la aplicacion, el escope restringe que paginas se pueden ver cuando se aplica el manifiesto
+//
+
+//en la consola comenzamos a trabajar con lighthouse, que tiene una informacion que es como un reporte
+//que se pide la pagina, que dice generate report, y perfomance, accessibility, buenas practicas
+//y si es una AWP, nos centralizamos en la parte de instalable(usamos como referencia a airhorner.com)
+
+//instalacion:
+/**1- prepara la instalacion, decir que voy a instalar (commit)
+ * 2-efectuar la instalacion, realizar el copiado de los archivos (push).
+ * equivalencia de una AWP
+ * 1-indicar al browser que esta ante una sitio instalable
+ * agregar el "añadir a la pantalla de inicio"
+ * 2-copiar los archivos estaticos a la cache(clase6)
+ * 
+ * --------------creacione del manifest.json
+ * 1-definir las propiedades generales(name,short_name, display,scope,statr_url, theme_color, background_color)
+ * 2-crear los iconos(512x512 y 192x192)
+ * 
+ * 
+ * la primera vez que tiramos el reporte tira dos errores uno del display
+ * ,por que tenemos browser, lo cambiamos por standalone, y otra que no registra ningun service worker.
+ * 
+ * 
+ * en el segundo reporte salta que falta el servie worker, que el service worker, es una secuencia de comandps
+ * que se ejecuta en segundo plano, pueden haber varios service woker, esta bueno xq se ejecuta en segundo plano y mejora
+ * la interaccion y la perfomance de la pagina,
+ * por un lado tenemos los preceso principales, que esta la carga de la pagina y la ejecucion de algun script, en paralelos
+ * proceso asincronicos como ajax o promises
+ * y por otro lado estan los service worker en paralelo fuera del proceso principal totalemnte independiente.
+ * 
+ * en la clase 5 se ve mas profundo sobre los service worker que son como un filtro
+ * ahora hacemos un worker vacio para pder hacer la app instalable, 
+ * asi que creamos otro archivo (sw.js) y ahi codificamos el service worker
+ * 
+ *  */
+
+
---
+++ sw.js
@@ -0,0 +1,51 @@
+/**aca primero hay que registrar el service worker, verificando en el objeto navigation asi
+ * que se trabaja con esas propiedades.
+ * y tambien trabjar con los eventos del serviceworker que dentro esta el install, el activate y el fetch.
+ * 
+ * 
+ * 
+ */
+
+//proceso
+//fetch: se ejecuta alinterceptar una peticion saliente, es el primero es en donde va la mayor cantidad de logica
+//manda todo al cache.
+//palabra reservada que hace referencia a un objeto seria como un this pero dentro de un worker se usa self
+self.addEventListener('fetch', e =>{
+
+}); //esto seria la parte de proceso luego sigue la de preparacion
+
+
+//preparacion:
+//install: se ejecuta una vez al principio, aca lo que hace es un copiado de archivos a la cache
+self.addEventListener('install', e=>{
+
+});
+
+//activate: este evento se ejecuta al haber un cambio en este archivo, esto realiza una actualizacion de los archivos de la cache
+self.addEventListener('activate', e=>{
+
+});
+//resumen: hacer instalable una aplicacion
+
+// 1 indicar al browser que este sitio es un sitio instalable
+/**
+ * -indicar el nombre de la aplicacion
+ * -indicar el incono
+ * -defifinir la forma que se visualiza el sitio
+ * -manifest.json
+ * 
+ * 2-programar la logica necesaria para efectuar la instalacion
+ * -crear el proceso de copiado de archivos a la cache
+ * -serviceworker/cache
+ */
+
+
+
+
+
+
+
+
+
+
+
